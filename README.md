a3421a95f6161c91eba73adbd4a04afebfcfc342
Sreenath N <tacticiankerala@gmail.com>
2013-07-31 02:00:21 +0530

    Keeping track of already evaluated tokens to avoid infinite loop

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Transitive-Dependencies-Problem
 ===============================
 
 Initial solution for the transitive dependency problem using ruby. Read more about transitive dependency problem in CodeKata - http://codekata.pragprog.com/2007/01/kata_eighteen_t.html. 
+
--- src/dependencies.rb
+++ src/dependencies.rb
@@ -9,15 +9,16 @@ class Dependencies
   end
   
   def dependencies_for token_name
+    @evaluated = []
     (find_dependencies_for token_name).sort
   end
   
-  def find_dependencies_for token_name, evaluated = []
+  def find_dependencies_for token_name
     dependencies = @dependency_store[token_name.to_sym] || []
-    evaluated << token_name
+    @evaluated << token_name
     dependencies.each do |dependency|
-      next if evaluated.include?(dependency)
-      dependencies = dependencies | find_dependencies_for(dependency,evaluated) 
+      next if @evaluated.include?(dependency)
+      dependencies = dependencies | find_dependencies_for(dependency)
     end
     dependencies
   end
